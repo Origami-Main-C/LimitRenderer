986677fe8883c78f5399ef8c9949628a5eb473b8
Origami-Main-C <origami-main-c@outlook.com>
2023-02-11 16:42:14 +0800

    update ignore

--- project/src/Application.cpp
+++ project/src/Application.cpp
@@ -10,11 +10,13 @@ int main() {
     //Init window
     Window gamesystem(800, 600, const_cast<char*>("GameSystem"), const_cast<char*>("../project/icon/icon.png"));
     Shader shader("../project/shader/rectangleShader/vertexShader.glsl", "../project/shader/rectangleShader/fragmentShader.glsl");
+    Texture texture("../project/texture/icon.png");
     float vertices[] = {
-            0.5f, 0.5f, 0.0f,        // top right
-            0.5f, -0.5f, 0.0f,       // bottom right
-            -0.5f, -0.5f, 0.0f,      // bottom left
-            -0.5f, 0.5f, 0.0f ,    // top left
+            // positions          // colors           // texture coords
+            0.5f,  -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+            0.5f, 0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+            -0.5f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+            -0.5f,  -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
     unsigned int indices[] = {
             0, 1, 3,  // first Triangle
@@ -28,16 +30,22 @@ int main() {
     vao.Bind();
     vbo.Bind();
     ebo.Bind();
-    vao.Layout(0, 3, GL_FLOAT, 3 * sizeof(float), 0);
+    vao.Layout(0, 3, GL_FLOAT, 8 * sizeof(float), 0);
+    vao.Layout(1, 3, GL_FLOAT, 8 * sizeof(float), 3*sizeof(float));
+    vao.Layout(2, 2, GL_FLOAT, 8 * sizeof(float), 6*sizeof(float));
     //colors
     ImVec4 color = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
     ImVec4 rectangle_color = ImVec4(0.2f, 0.3f, 0.5f, 1.0f);
+    shader.use();
+    shader.setInt("texture1",0);
     while (!gamesystem.ShouldClose()) {
         processInput(gamesystem);
         glClearColor(color.x, color.y, color.z, color.w);
         glClear(GL_COLOR_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
+        texture.Bind();
         shader.use();
-        shader.setVec4("color", glm::vec4(rectangle_color.x, rectangle_color.y, rectangle_color.z, rectangle_color.w));
+        shader.setVec4("ourColor", glm::vec4(rectangle_color.x, rectangle_color.y, rectangle_color.z, rectangle_color.w));
         vao.Bind();
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         vao.Unbind();
@@ -45,7 +53,7 @@ int main() {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        ImGui::Begin("Color Edit");
+        ImGui::Begin("Color Edit", nullptr);
         ImGui::ColorEdit3("background color", (float *) &color);
         ImGui::End();
         ImGui::Begin("Color Edit2");

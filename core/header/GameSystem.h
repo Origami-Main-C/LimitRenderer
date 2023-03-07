14dbcce3327a5d3635cbad5d04d84553fcf562fa
Origami-Main-C <origami-main-c@outlook.com>
2023-03-07 23:17:49 +0800

    reformat

--- core/header/GameSystem.h
+++ core/header/GameSystem.h
@@ -1,18 +1,19 @@
 #pragma once
-// Included all the headers
+//Included all the headers
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <GameSystem/Camera.h>
-#include <GameSystem/File.h>
+#include <iostream>
+#include <Project_Config.h>
 #include <GameSystem/Gui.h>
+#include <imfilebrowser.h>
+#include <GameSystem/Normal.h>
+#include <GameSystem/Window.h>
+#include <GameSystem/Camera.h>
+#include <GameSystem/Shader.h>
+#include <stb_image.h>
+#include <GameSystem/Texture.h>
+#include <GameSystem/File.h>
 #include <GameSystem/Log.h>
+#include <GameSystem/Objects.h>
 #include <GameSystem/Mesh.h>
 #include <GameSystem/Model.h>
-#include <GameSystem/Normal.h>
-#include <GameSystem/Objects.h>
-#include <GameSystem/Shader.h>
-#include <GameSystem/Texture.h>
-#include <GameSystem/Window.h>
-#include <Project_Config.h>
-#include <glad/glad.h>
-#include <iostream>
-#include <stb_image.h>
